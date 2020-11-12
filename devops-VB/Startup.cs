92c781acf8445146d28c6f83dddbe6393feb5677
ahmed <abamahriz@gmail.com>
2020-11-11 22:52:23 -0500

    new change

--- devops-VB/Startup.cs
+++ devops-VB/Startup.cs
@@ -24,6 +24,7 @@ namespace devops_VB
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            // change no 1
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
